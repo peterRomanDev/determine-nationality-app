30e4bc6ff8b340b2a0ddd08db914e2ab3e82bcaf
Peter Roman <peter.roman24@gmail.com>
2023-12-08 03:58:08 +0100

    Improve HTML, CSS and JS

--- scripts/app.js
+++ scripts/app.js
@@ -5,41 +5,69 @@ const sidebarTooltip = document.querySelector('.sidebar-tooltip');
 const dashboardWrapper = document.querySelector('.dashboard-wrapper');
 
 app.addEventListener('click', e => {
-    if(e.target.classList.contains('sidebar--btn-toggle')) {
+    // show/hide sidebar button clicked
+    if(e.target.classList.contains('sidebar-btn-toggle')) {
+        // if the sidebar is hidden...
         if(sidebarWrapper.classList.contains('sidebar-hidden')) {
+            // show sidebar
             sidebarWrapper.classList.remove('sidebar-hidden');
+
+            // update the text of the tooltip message
             sidebarTooltip.textContent = 'Hide sidebar';
 
+            // shrink the dashboard
             dashboardWrapper.classList.remove('dashboard-full-screen');
-        } else {
+        }
+        // if the sidebar is visible...
+        else {
+            // hide sidebar
             sidebarWrapper.classList.add('sidebar-hidden');
+
+            // update the text of the tooltip message
             sidebarTooltip.textContent = 'Show sidebar';
 
+            // extend the dashboard to full-screen
             dashboardWrapper.classList.add('dashboard-full-screen');
         }
-    } else if(e.target.classList.contains('sidebar__options-btn')) {
-        const sidebarOptionsBtns = document.querySelectorAll('.sidebar__options-btn');
-
+    }
+    // sidebar option button clicked
+    else if(e.target.classList.contains('sidebar-options__btn')) {
+        // make all the buttons not highlighted
+        const sidebarOptionsBtns = document.querySelectorAll('.sidebar-options__btn');
         sidebarOptionsBtns.forEach(btn => btn.classList.remove('sidebar-option-active'));
-        e.target.classList.add('sidebar-option-active');
-    } else if(e.target.classList.contains('user')) {
-        const users = document.querySelectorAll('.user');
         
+        // highlight the option button that is clicked
+        e.target.classList.add('sidebar-option-active');
+    }
+    // user clicked
+    else if(e.target.classList.contains('list-of-users__user')) {
+        // make all the users not highlighted
+        const users = document.querySelectorAll('.list-of-users__user');
         users.forEach(user => user.classList.remove('user-active'));
+        
+        // highlight the user that is clicked
         e.target.classList.add('user-active');
     }
 });
 
 app.addEventListener('pointerover', e => {
-    if(e.target.classList.contains('sidebar--btn-toggle')) {
+    // the show/hide button is hovered over
+    if(e.target.classList.contains('sidebar-btn-toggle')) {
+        // display the tooltip message
         sidebarTooltip.classList.remove('d-none');
+        
+        // show the tooltip message
         setTimeout(() => sidebarTooltip.classList.remove('transparent'), 125);
     }
 });
 
 app.addEventListener('pointerout', e => {
-    if(e.target.classList.contains('sidebar--btn-toggle')) {
+    // the cursor leaves the show/hide button
+    if(e.target.classList.contains('sidebar-btn-toggle')) {
+        // hide the tooltip message
         sidebarTooltip.classList.add('transparent');
+
+        // undisplay the tooltip message
         setTimeout(() => sidebarTooltip.classList.add('d-none'), 125);
     }
 });
