ce1eb4e5be97e8588dfff9b2d4ae8d6a26a93c98
Peter Roman <peter.roman24@gmail.com>
2025-01-21 15:52:30 +0100

    Fix bugs related to adding and deleting users from local storage, add comments

--- scripts/user-storage.js
+++ scripts/user-storage.js
@@ -4,37 +4,54 @@ export const storeUser = name => {
     const formattedName = formatName(name);
 
     let storedUsersJSON = localStorage.getItem('storedUsers');
-    const storedUsers = JSON.parse(storedUsersJSON);
 
-    if (storedUsers) {
+    if (!storedUsersJSON || storedUsersJSON === 'null') {
+        // if there are no stored users - if storedUsersJSON is falsy or if it is a string of 'null'
+        const storedUsers = [];
         storedUsers.push(formattedName);
-        
+
+        const storedUsersJSON = JSON.stringify(storedUsers);
+        localStorage.setItem('storedUsers', storedUsersJSON);
+    } else {
+        // if there are stored users
+        const storedUsers = JSON.parse(storedUsersJSON);
+        storedUsers.push(formattedName);
+
         storedUsersJSON = JSON.stringify(storedUsers);
         localStorage.setItem('storedUsers', storedUsersJSON);
     }
 };
 
 export const addStoredUsers = () => {
-    const storedUsersJSON = localStorage.getItem('storedUsers');
+    let storedUsersJSON = localStorage.getItem('storedUsers');
+
+    if (!storedUsersJSON || storedUsersJSON === 'null') return;
+    // return if there are no stored users - if storedUsersJSON is falsy or if it is a string of 'null'
+
+    // if there are stored users
     const storedUsers = JSON.parse(storedUsersJSON);
-    
-    if (storedUsers) {
-        storedUsers.forEach(user => addUser(user));
-    }
+    storedUsers.forEach(user => addUser(user));
 };
 
 export const deleteStoredUser = user => {
     let storedUsersJSON = localStorage.getItem('storedUsers');
-    const storedUsers = JSON.parse(storedUsersJSON);
 
-    if (storedUsers) {
-        storedUsers.forEach((storedUser, index) => {
-            if(storedUser === user) {
-                storedUsers.splice(index, 1);
-            }
-        });
-    }
+    if (!storedUsersJSON || storedUsersJSON === 'null') return;
+    // return if there are no stored users - if storedUsersJSON is falsy or if it is a string of 'null'
+
+    // if there are stored users
+    const storedUsers = JSON.parse(storedUsersJSON);
+    storedUsers.forEach((storedUser, index) => {
+        if(storedUser === user) {
+            storedUsers.splice(index, 1);
+        }
+    });
 
     storedUsersJSON = JSON.stringify(storedUsers);
     localStorage.setItem('storedUsers', storedUsersJSON);
+
+    if (storedUsers.length === 0) {
+        // if all users have been deleted from the local storage, remove the storedUsers entry from the local storage entirely
+        localStorage.removeItem('storedUsers');
+    }
 };
